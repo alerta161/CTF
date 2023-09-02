074e7c615a77bca9ad41d4833c9005efb985dec4
alerta_161 <sashaalerta161>
2023-09-02 12:44:09 +0200

    notes1

--- notes.py
+++ notes.py
@@ -4,7 +4,6 @@ import music21
 def convert_notes_to_melody(note_string):
     melody = music21.stream.Stream()
 
-    # Разделение введенных нот по пробелам и обработка каждой ноты
     notes = note_string.split()
 
     for note in notes:
@@ -15,13 +14,10 @@ def convert_notes_to_melody(note_string):
 
 
 if __name__ == "__main__":
-    # Получить ввод пользователя
-    note_input = input("Введите ноты (например, C4 D4 E4): ")
+    note_input = input("Enter notes (for example, C4 D4 E4): ")
 
-    # Конвертировать введенные ноты в мелодию
     melody = convert_notes_to_melody(note_input)
 
-    # Воспроизвести мелодию
     midi_file = melody.write('midi')
     mf = music21.midi.MidiFile()
     mf.open("output.mid", "wb")
